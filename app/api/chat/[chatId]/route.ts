dfb0d80434b6a8a8f0d05039d36a75079eda4757
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-07-26 20:42:37 +0200

    feat: add delete functionality

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { StreamingTextResponse, LangChainStream } from "ai";
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { Replicate } from "langchain/llms/replicate";
 import { CallbackManager } from "langchain/callbacks";
 import { NextResponse } from "next/server";
@@ -15,131 +15,160 @@ export async function POST(
   request: Request,
   { params }: { params: { chatId: string } }
 ) {
-  const { prompt } = await request.json();
-  const user = await currentUser();
+  try {
+    const { prompt } = await request.json();
+    const user = await currentUser();
 
-  if (!user || !user.firstName || !user.id) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
-
-  const identifier = request.url + "-" + user.id;
-  const { success } = await rateLimit(identifier);
-
-  if (!success) {
-    return new NextResponse("Rate limit exceeded", { status: 429 });
-  }
-
-  const companion = await prismadb.companion.update({
-    where: {
-      id: params.chatId
-    },
-    data: {
-      messages: {
-        create: {
-          content: prompt,
-          role: "user",
-          userId: user.id,
-        },
-      },
+    if (!user || !user.firstName || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-  });
 
-  if (!companion) {
-    return new NextResponse("Companion not found", { status: 404 });
-  }
+    const identifier = request.url + "-" + user.id;
+    const { success } = await rateLimit(identifier);
 
-  const name = companion.name;
-  const companion_file_name = name + ".txt";
+    if (!success) {
+      return new NextResponse("Rate limit exceeded", { status: 429 });
+    }
 
-  const companionKey = {
-    companionName: name!,
-    userId: user.id,
-    modelName: "llama2-13b",
-  };
-  const memoryManager = await MemoryManager.getInstance();
-
-  const records = await memoryManager.readLatestHistory(companionKey);
-  if (records.length === 0) {
-    await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
-  }
-  await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
-
-  // Query Pinecone
-
-  const recentChatHistory = await memoryManager.readLatestHistory(companionKey);
-
-  // Right now the preamble is included in the similarity search, but that
-  // shouldn't be an issue
-
-  const similarDocs = await memoryManager.vectorSearch(
-    recentChatHistory,
-    companion_file_name
-  );
-
-  let relevantHistory = "";
-  if (!!similarDocs && similarDocs.length !== 0) {
-    relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
-  }
-  const { handlers } = LangChainStream();
-  // Call Replicate for inference
-  const model = new Replicate({
-    model:
-      "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
-    input: {
-      max_length: 2048,
-    },
-    apiKey: process.env.REPLICATE_API_TOKEN,
-    callbackManager: CallbackManager.fromHandlers(handlers),
-  });
-
-  // Turn verbose on for debugging
-  model.verbose = true;
-
-  const resp = String(
-    await model
-      .call(
-        `
-       ONLY generate NO more than three sentences as ${name}. DO NOT generate more than three sentences. 
-
-       ${companion.instructions}
-
-       Below are relevant details about ${name}'s past and the conversation you are in.
-       ${relevantHistory}
-
-
-       ${recentChatHistory}\n${name}:`
-      )
-      .catch(console.error)
-  );
-
-  const cleaned = resp.replaceAll(",", "");
-  const chunks = cleaned.split("\n");
-  const response = chunks[0];
-
-  await memoryManager.writeToHistory("" + response.trim(), companionKey);
-  var Readable = require("stream").Readable;
-
-  let s = new Readable();
-  s.push(response);
-  s.push(null);
-  if (response !== undefined && response.length > 1) {
-    memoryManager.writeToHistory("" + response.trim(), companionKey);
-
-    await prismadb.companion.update({
+    const companion = await prismadb.companion.update({
       where: {
         id: params.chatId
       },
       data: {
         messages: {
           create: {
-            content: response.trim(),
-            role: "system",
+            content: prompt,
+            role: "user",
             userId: user.id,
           },
         },
       }
     });
-  }
 
-  return new StreamingTextResponse(s);
-}
+    if (!companion) {
+      return new NextResponse("Companion not found", { status: 404 });
+    }
+
+    const name = companion.id;
+    const companion_file_name = name + ".txt";
+
+    const companionKey = {
+      companionName: name!,
+      userId: user.id,
+      modelName: "llama2-13b",
+    };
+    const memoryManager = await MemoryManager.getInstance();
+
+    const records = await memoryManager.readLatestHistory(companionKey);
+    if (records.length === 0) {
+      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
+    }
+    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
+
+    // Query Pinecone
+
+    const recentChatHistory = await memoryManager.readLatestHistory(companionKey);
+
+    // Right now the preamble is included in the similarity search, but that
+    // shouldn't be an issue
+
+    const similarDocs = await memoryManager.vectorSearch(
+      recentChatHistory,
+      companion_file_name
+    );
+
+    let relevantHistory = "";
+    if (!!similarDocs && similarDocs.length !== 0) {
+      relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+    }
+    const { handlers } = LangChainStream();
+    // Call Replicate for inference
+    const model = new Replicate({
+      model:
+        "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+      input: {
+        max_length: 2048,
+      },
+      apiKey: process.env.REPLICATE_API_TOKEN,
+      callbackManager: CallbackManager.fromHandlers(handlers),
+    });
+
+    // Turn verbose on for debugging
+    model.verbose = true;
+
+    const resp = String(
+      await model
+        .call(
+          `
+        ONLY generate NO more than three sentences as ${name}. DO NOT generate more than three sentences. 
+
+        ${companion.instructions}
+
+        Below are relevant details about ${name}'s past and the conversation you are in.
+        ${relevantHistory}
+
+
+        ${recentChatHistory}\n${name}:`
+        )
+        .catch(console.error)
+    );
+
+    const cleaned = resp.replaceAll(",", "");
+    const chunks = cleaned.split("\n");
+    const response = chunks[0];
+
+    await memoryManager.writeToHistory("" + response.trim(), companionKey);
+    var Readable = require("stream").Readable;
+
+    let s = new Readable();
+    s.push(response);
+    s.push(null);
+    if (response !== undefined && response.length > 1) {
+      memoryManager.writeToHistory("" + response.trim(), companionKey);
+
+      await prismadb.companion.update({
+        where: {
+          id: params.chatId
+        },
+        data: {
+          messages: {
+            create: {
+              content: response.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        }
+      });
+    }
+
+    return new StreamingTextResponse(s);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { chatId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const companion = await prismadb.companion.delete({
+      where: {
+        userId,
+        id: params.chatId
+      }
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[CHAT_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
